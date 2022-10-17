f7df369bf658c27ce18269fd3078445104cc41ef
김재만 <daga4242@gmail.com>
2022-10-17 16:35:47 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { getMessaging, onMessage } from "firebase/messaging";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyD8bAXN6uWXdBvWnGMYemYnEtU8gJmLdC4",
+  authDomain: "fcm-push-7bced.firebaseapp.com",
+  projectId: "fcm-push-7bced",
+  storageBucket: "fcm-push-7bced.appspot.com",
+  messagingSenderId: "211185700909",
+  appId: "1:211185700909:web:a6ba1715d74041976068a4",
+});
+
+const messaging = getMessaging(firebaseApp);
+onMessage(messaging, (payload) => {
+  console.log("Message received. ", payload);
+  // ...
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        console.log("granted");
+      }
+    });
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { getMessaging } from "firebase/messaging";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyD8bAXN6uWXdBvWnGMYemYnEtU8gJmLdC4",
+  authDomain: "fcm-push-7bced.firebaseapp.com",
+  projectId: "fcm-push-7bced",
+  storageBucket: "fcm-push-7bced.appspot.com",
+  messagingSenderId: "211185700909",
+  appId: "1:211185700909:web:a6ba1715d74041976068a4",
+});
+
+// Initialize Firebase
+const messaging = getMessaging(firebaseApp);
