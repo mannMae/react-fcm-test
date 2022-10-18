7b8997e45d2e049cf2a7b37de020c21ddca5a228
김재만 <daga4242@gmail.com>
2022-10-18 09:51:36 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { useEffect } from "react";
-import { getMessaging, onMessage } from "firebase/messaging";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+// import { getMessaging, onMessage } from "firebase/messaging";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
 
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyD8bAXN6uWXdBvWnGMYemYnEtU8gJmLdC4",
-  authDomain: "fcm-push-7bced.firebaseapp.com",
-  projectId: "fcm-push-7bced",
-  storageBucket: "fcm-push-7bced.appspot.com",
-  messagingSenderId: "211185700909",
-  appId: "1:211185700909:web:a6ba1715d74041976068a4",
-});
-
-// const messaging = getMessaging(firebaseApp);
-// onMessage(messaging, (payload) => {
-//   console.log("Message received. ", payload);
-//   // ...
+// const firebaseApp = initializeApp({
+//   apiKey: "AIzaSyD8bAXN6uWXdBvWnGMYemYnEtU8gJmLdC4",
+//   authDomain: "fcm-push-7bced.firebaseapp.com",
+//   projectId: "fcm-push-7bced",
+//   storageBucket: "fcm-push-7bced.appspot.com",
+//   messagingSenderId: "211185700909",
+//   appId: "1:211185700909:web:a6ba1715d74041976068a4",
 // });
+
+// // const messaging = getMessaging(firebaseApp);
+// // onMessage(messaging, (payload) => {
+// //   console.log("Message received. ", payload);
+// //   // ...
+// // });
 
 function App() {
   useEffect(() => {
