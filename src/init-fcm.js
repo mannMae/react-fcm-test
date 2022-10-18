5e61cbae38724924d9ba97d3105d1d8d9556ab4c
김재만 <daga4242@gmail.com>
2022-10-18 10:27:42 +0900

    firebase version convert

--- src/init-fcm.js
+++ src/init-fcm.js
@@ -1,10 +1,7 @@
-import * as firebase from "firebase/app";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
-import { getMessaging, onMessage } from "firebase/messaging";
+import firebase from "firebase/app";
 import "firebase/messaging";
 
-// const initializedFirebaseApp = firebase.initializeApp({
-const firebaseApp = initializeApp({
+const initializedFirebaseApp = firebase.initializeApp({
   // Project Settings => Add Firebase to your web app
   apiKey: "AIzaSyD8bAXN6uWXdBvWnGMYemYnEtU8gJmLdC4",
   authDomain: "fcm-push-7bced.firebaseapp.com",
@@ -13,17 +10,19 @@ const firebaseApp = initializeApp({
   messagingSenderId: "211185700909",
   appId: "1:211185700909:web:a6ba1715d74041976068a4",
 });
-
-// const messaging = initializedFirebaseApp.messaging();
-
-const messaging = getMessaging(firebaseApp);
-console.log(messaging);
+const messaging = initializedFirebaseApp.messaging();
 messaging.usePublicVapidKey(
   // Project Settings => Cloud Messaging => Web Push certificates
   "BFaHfNo6Cp93RnX9fWKwb5o0sOgIWfo8uvwWZZKddy08fY814uf3Pllr0eMDQShBKIETkUoXRMvSFTNAu-BqyTM"
 );
 export { messaging };
 
+// import { getMessaging, onMessage } from "firebase/messaging";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+
+// const firebaseApp = initializeApp({});
+
+// const messaging = getMessaging(firebaseApp);
 // onMessage(messaging, (payload) => {
 //   console.log("Message received. ", payload);
 //   // ...
